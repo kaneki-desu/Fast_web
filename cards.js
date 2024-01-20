31186d2ba7c6ad074e952ac20a7591b9045c90e1
Sibajit Mazumder <148204596+kaneki-desu@users.noreply.github.com>
2024-01-20 07:50:14 +0530

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,112 +1,104 @@
-class Card{
-    constructor({imgLocation,onDismiss}){
-        this.imgLocation= imgLocation;
+class Card {
+    constructor({ imgLocation, onDismiss }) {
+        this.imgLocation = imgLocation;
         this.#init();
-        this.onDismiss=onDismiss;
+        // this.onDismiss = onDismiss;
+        this.element.style.transition = '0.6s';
+
     }
 
-//private properties
-#startingPoint;
-#offsetX;
-#offsetY;
-//private methods
-#init = () =>{
-    const card = document.createElement('div');
-    card.classList.add('card');
-    const img = document.createElement('img');
-    img.src=this.imgLocation;
-    card.append(img);
+    //private properties
+    #startingPoint;
+    #offsetX;
+    #offsetY;
+    //private methods
+    #init = () => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        const img = document.createElement('img');
+        img.src = this.imgLocation;
+        card.append(img);
 
-    const txt =document.createElement('div');
-    txt.classList.add('text');
-    card.append(txt);
+        const txt = document.createElement('div');
+        txt.classList.add('text');
+        card.append(txt);
 
-    this.element = card;
-    console.log(this);
-    
-    this.#listenTOMousevents();
-}
+        this.element = card;
+        console.log(this.element);
+        console.log(document.getElementsByClassName('card')[0]);
+        this.#listenTOMousevents()
+    }
 
-#listenTOMousevents = ()=>{
-    // mousedown
-    this.element.addEventListener('mousedown',(e)=>{
-        const{clientX, clientY} = e ;        
-        this.#startingPoint={ x:clientX , y: clientY};
-        document.addEventListener('mousemove', this.#handleMouseMove)
-    })
+    #listenTOMousevents = () => {
+        // mousedown
+        this.element.addEventListener('mousedown', (e) => {
+            const { clientX, clientY } = e;
+            this.#startingPoint = { x: clientX, y: clientY };
+            document.addEventListener('mousemove', this.#handleMouseMove)
+        })
 
-    // prevent drag
-    this.element.addEventListener('dragstart',(e)=>{
-        e.preventDefault();
-    });
+        // prevent drag
+        this.element.addEventListener('dragstart', (e) => {
+            e.preventDefault();
+        });
 
-    //mouseup
-    this.element.addEventListener('mouseup',this.#handleMouseUp)
+        //mouseup
+        this.element.addEventListener('mouseup', this.#handleMouseUp)
 
         // this.element.style.transition="0.6s";
     }
-#handleMouseUp =  (e)=>{
-        this.#startingPoint= null;
+    #handleMouseUp = (e) => {
+        this.#startingPoint = null;
         document.removeEventListener('mousemove', this.#handleMouseMove);
-}
-#handleMouseMove =(e) =>{
+    }
+    #handleMouseMove = (e) => {
         if (!this.#startingPoint) return;
-        const { clientX, clientY} = e;
-        this.#offsetX =clientX - this.#startingPoint.x;
-        this.#offsetY= clientY - this.#startingPoint.y;
-        this.element.style.transform =`translate( ${this.#offsetX}px, 0px )`;
-        //dismiss card when moving too far away
-        // console.log(this.#offsetX);
+        const { clientX, clientY } = e;
+        this.#offsetX = clientX - this.#startingPoint.x;
+        this.#offsetY = clientY - this.#startingPoint.y;
+        this.element.style.transform = `translate( ${this.#offsetX}px, 0px )`;
         
-        if((Math.abs(this.#offsetX) > 230)){
-            // this.element.style.left='100px'
-            // 
-            // this.element.style.opacity='0'
-            const direction = this.#offsetX > 0 ? 1:-1;
+        //dismiss card when moving too far away
+
+        if ((Math.abs(this.#offsetX) > 230)) {
+            const direction = this.#offsetX > 0 ? 1 : -1;
             this.#dismiss();
         }
-        
+
     }
-#dismiss=(direction)=>{
-        this.#startingPoint=null;
-        document.removeEventListener('mouseup',this.#handleMouseUp)
-        document.removeEventListener("mousemove",this.#handleMouseMove)
-        this.element.style.transition='1.5s'
-        this.element.style.transform= `translate(1000px,0px)`
-        this.element.style.opacity= `0`;
-        setTimeout(()=>{
+    #dismiss = (direction) => {
+        this.#startingPoint = null;
+        document.removeEventListener('mouseup', this.#handleMouseUp)
+        document.removeEventListener("mousemove", this.#handleMouseMove)
+        this.element.style.transition = '1.5s'
+        this.element.style.transform = `translate(1000px,0px)`
+        this.element.style.opacity = `0`;
+        setTimeout(() => {
             this.element.remove();
-            this.element.style.transform= `translate(calc(-15% * var(--y)),0px)`;
-            this.element.style.transition='0.6s';
-            this.element.style.opacity= `1`;
+            this.element.style.transform = `translate(calc(-15% * var(--y)),0px)`;
+            this.element.style.transition = '0.6s';
+            this.element.style.opacity = `1`;
             //updates --y value
-            container.append(this.element);
-            this.element.style.setProperty('--y', this.#sety());
-            // console.log(this.#sety());
+            console.log(this.#sety());
+            this.element.style.setProperty('--y', y_arr[y_arr.length - 1]);
             
-        },2000)
-        if (typeof this.onDismiss ==='function') {
+            setTimeout(()=>{
+                container.append(this.element);
+            },200)
+            // console.log(this.#sety());
+
+        }, 800)
+        if (typeof this.onDismiss === 'function') {
             this.onDismiss();
         }
     }
-#sety = () =>{
+    #sety = () => {
         for (const item of (document.getElementsByClassName('card'))) {
-            console.log(item, 'item');
             if (item.style.getPropertyValue('--y') > 0) {
-                console.log(item.style.getPropertyValue('--y') );
-                item.style.setProperty('--y', (item.style.getPropertyValue('--y') - 1))                
-                console.log(item.style.getPropertyValue('--y'), 'heyy' );
-            }
-            else {
-                item.style.setProperty('--y', y_arr.length - 1) ;
-                console.log(item.style.getPropertyValue('--y'), 'heyyyy' );
-                console.log('else');
-                
+                item.style.setProperty('--y', (item.style.getPropertyValue('--y') - 1))
             }
         }
-        console.log(y_arr[y_arr.length-1]);
-        return y_arr[y_arr.length-1];
+        
     }
-
+    
 }
-
